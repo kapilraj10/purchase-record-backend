9a4113bc467c796b883916ff0850b5ffdac16511
kapil <kapilrajkc10@gmail.com>
2025-11-09 23:46:02 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use("/api", routes);
 
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: "https://purchase-system-client.vercel.app"
   
   ,
   credentials: true,
